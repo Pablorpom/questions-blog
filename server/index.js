d9d6639891161da13206a2fd069b29db4fb3dc26
Pablo Restrepo M <pablorestrepom757@gmail.com>
2021-11-16 23:02:59 -0500

    Adding CORS

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const Questions = require('./models/Questions')
 const app = express()
 const port = 3000
 
+app.use(cors())
 app.use(bodyParser.json());
 
 const questions = new Questions();
